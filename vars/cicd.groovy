f604561b9385a8d393dc3e34dfa5cc5d01ede2f2
PREETIGHATAGE <164339733+PREETIGHATAGE@users.noreply.github.com>
2024-04-12 14:50:29 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,17 +2,4 @@ def newGit(repo)
 {
 git "${repo}"
 }
-def newMaven()
-{
-sh "mvn package"
-}
-def newDeploy(http://172.31.86.203:8080,testapp)
-{
-deploy adapters: [tomcat9(credentialsId: '8a3325e9-d38f-4e77-be7d-f009417b0ffb', path: '', url: "${http://172.31.86.203:8080}")], contextPath: "${testapp}", war: '**/*.war'
-}
-def newTest(pipeline)
-{
-echo "${pipeline}
-sh "java -jar /home/ubuntu/.jenkins/workspace/${pipeline}/testing.jar"
-}
 
