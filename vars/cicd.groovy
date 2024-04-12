b945b37f58b69ff8bd816314f765762761691604
PREETIGHATAGE <164339733+PREETIGHATAGE@users.noreply.github.com>
2024-04-12 14:37:37 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -4,11 +4,11 @@ git "${https://github.com/PREETIGHATAGE/libraries.git}"
 }
 def newMaven()
 {
-sh 'mvn package'
+sh "mvn package"
 }
 def newDeploy(http://172.31.86.203:8080,testapp)
 {
-deploy adapters: [tomcat9(credentialsId: '8a3325e9-d38f-4e77-be7d-f009417b0ffb', path: '', url: 'http://172.31.86.203:8080')], contextPath: 'testapp', war: '**/*.war'
+deploy adapters: [tomcat9(credentialsId: '8a3325e9-d38f-4e77-be7d-f009417b0ffb', path: '', url: "${http://172.31.86.203:8080}")], contextPath: "${testapp}", war: '**/*.war'
 }
 def newTest(pipeline)
 {
