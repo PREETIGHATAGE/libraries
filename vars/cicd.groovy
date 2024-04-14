68f888bfa052d1ad6f5068b6e3e634ded0471279
PREETIGHATAGE <164339733+PREETIGHATAGE@users.noreply.github.com>
2024-04-14 12:24:58 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,4 +6,7 @@ def newMaven()
 {
 sh "mvn package"
 }
-
+def newDeploy(ip, appname)
+{
+deploy adapters: [tomcat9(credentialsId: '8a3325e9-d38f-4e77-be7d-f009417b0ffb', path: '', url: "${http://172.31.86.203:8080}")], contextPath: "${testapp}", war: '**/*.war'
+}
